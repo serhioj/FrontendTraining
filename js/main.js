097dba44246cc6e4fd9f7111db79e4d293018e6e
serhioj <goryaynov_serzh@mail.ru>
2024-10-15 20:57:22 +0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,46 @@
+var swiper = new Swiper('.swiper', {
+    loop: true, // Зацикливание
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 1,
+    spaceBetween: 0,
+    setWrapperSize: false,
+
+    // autoplay: {
+    //     delay: 3000,
+    // },
+});
+
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find('.burger-menu_button', '.burger-menu_lines');
+    let links = menu.find('.burger-menu_link');
+    let overlay = menu.find('.burger-menu_overlay');
+
+    button.on('click', (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    links.on('click', () => toggleMenu());
+    overlay.on('click', () => toggleMenu());
+
+    function toggleMenu() {
+        menu.toggleClass('burger-menu_active');
+
+        if (menu.hasClass('burger-menu_active')) {
+            $('body').css('overlow', 'hidden');
+        } else {
+            $('body').css('overlow', 'visible');
+        }
+    }
+}
+
+burgerMenu('.burger-menu');
